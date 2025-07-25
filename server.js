090c0bc471d481677afcb67234491a480a21fada
Asma battikhi <85161475+asmabatttikhi@users.noreply.github.com>
2025-07-25 18:19:54 +0300

    Update server.js
    1

--- server.js
+++ server.js
@@ -25,6 +25,34 @@ app.get('/', (req, res) => {
   res.send('API يعمل 🎉');
 });
 
+
+// تحديث إعلان
+app.put('/api/ads/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const updatedAd = req.body;
+
+  const index = ads.findIndex((ad) => ad.id === id);
+  if (index !== -1) {
+    ads[index] = { ...ads[index], ...updatedAd };
+    res.json(ads[index]);
+  } else {
+    res.status(404).json({ message: 'الإعلان غير موجود' });
+  }
+});
+
+// حذف إعلان
+app.delete('/api/ads/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = ads.findIndex((ad) => ad.id === id);
+  if (index !== -1) {
+    ads.splice(index, 1);
+    res.status(204).send(); // No Content
+  } else {
+    res.status(404).json({ message: 'الإعلان غير موجود' });
+  }
+});
+
+
 app.listen(port, () => {
   console.log(`API شغال على http://localhost:${port}`);
 });
